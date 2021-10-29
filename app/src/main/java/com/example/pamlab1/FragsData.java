8275c68cc3a52ca24eb825e3b31552e63f78a614
Artur <Arturlispl@wp.pl>
2021-10-29 18:40:23 +0200

    laboratorium 4

---
+++ app/src/main/java/com/example/pamlab1/FragsData.java
@@ -0,0 +1,10 @@
+package com.example.pamlab1;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class FragsData extends ViewModel {
+
+    public final MutableLiveData<Integer> counter = new MutableLiveData<>(0);
+
+}
